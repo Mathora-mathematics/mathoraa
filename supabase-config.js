79bcbbbbfa025bf71d44a4c7ff506eb9aa23a7c5
Mathora <121544764+Mathora-mathematics@users.noreply.github.com>
2026-08-05 14:11:45 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-const MATHORA_SUPABASE_URL = "https://ckmwaxoewylaaahudwtk.supabase.co";
-const MATHORA_SUPABASE_ANON_KEY = "sb_publishable_vbMrWIRqK96_6_51pR6QMw_OlSGqTjJ";
-window.mathoraSupabase = window.supabase.createClient(MATHORA_SUPABASE_URL, MATHORA_SUPABASE_ANON_KEY);
